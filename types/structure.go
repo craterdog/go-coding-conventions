90aa1d61ea31fbfd9d73bbf21c690ff9df402790
Derk Norton <derk.norton@gmail.com>
2023-11-23 11:03:40 -0700

    Added more code templates.

--- types/structure.go
+++ types/structure.go
@@ -1,20 +1,5 @@
-/*******************************************************************************
- *   Copyright (c) 2009-2022 Crater Dog Technologies™.  All Rights Reserved.   *
- *******************************************************************************
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               *
- *                                                                             *
- * This code is free software; you can redistribute it and/or modify it under  *
- * the terms of The MIT License (MIT), as published by the Open Source         *
- * Initiative. (See http://opensource.org/licenses/MIT)                        *
- *******************************************************************************/
-
-package <packageName>
-
-// STRUCTURE DEFINITIONS
-
 // This type defines the structure of...
 type <StructureName>_ struct {
 	<AttributeName> <AbstractType>
 	...
 }
-...
--- types/type.go
+++ types/type.go
@@ -1,3 +1,28 @@
+/*******************************************************************************
+ *   Copyright (c) 2009-2023 Crater Dog Technologies™.  All Rights Reserved.   *
+ *******************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               *
+ *                                                                             *
+ * This code is free software; you can redistribute it and/or modify it under  *
+ * the terms of The MIT License (MIT), as published by the Open Source         *
+ * Initiative. (See http://opensource.org/licenses/MIT)                        *
+ *******************************************************************************/
+
+package <packageName>
+
+import (
+	fmt "fmt"
+)
+
+// STRUCTURE DEFINITIONS
+
+// This type defines the structure of...
+type <StructureName>_ struct {
+	<AttributeName> <AbstractType>
+	...
+}
+...
+
 // CLASS DEFINITIONS
 
 // This private concrete type implements the <ClassName>Like interface.
