b4e11a438d62a06c1ddd6f60dfec52db70fd6cbb
Derk Norton <derk.norton@gmail.com>
2023-11-25 14:17:09 -0700

    Updated the package.go template.

--- packages/package.go
+++ packages/package.go
@@ -13,8 +13,9 @@ This package defines...
 For detailed documentation on this package refer to the wiki:
  * https://github.com/craterdog/<repository-name>/wiki
 
-All UpperCase constant, type, and function names are visible to other projects
-that import this package. All lowercase names are private to this package.
+All UpperCase type, constant, abstraction, class and function names are visible
+to other projects that import this package. All lowercase names are private to
+this package.
 */
 package <packageName>
 
@@ -23,40 +24,96 @@ import (
 	...
 )
 
-// PACKAGE CONSTANTS
-
-// This constant represents...
-const <ConstantName> =...
-...
-
-// This private constant represents...
-const <privateConstantName> =...
-...
-
 // PACKAGE TYPES
 
-// This type alias represents...
-type <TypeName> <primitiveType>
+// Primitive Types
 
-// This singleton defines a unique name space for the <className> class.
+// This type definition represents...
+type <TypeName> <primitiveType>
+...
+
+// Structured Types
+
+// This structured type defines the attributes for a <StructureName> entity.
+type <StructureName> struct {
+	<AttributeName> <AttributeType>
+	...
+}
+...
+
+// PACKAGE CONSTANTS
+
+// Public Constants
+
+// This constant represents...
+const <ConstantName> = <ConstantValue>
+...
+
+// Private Constants
+
+// This private constant represents...
+const <privateConstantName> = <PrivateConstantValue>
+...
+
+// PACKAGE ABSTRACTIONS
+
+// Abstract Types
+
+// This abstract type defines the set of abstract interfaces that must be
+// supported by all <TypeName>Like types.
+type <TypeName>Like interface {
+	<InterfaceName>
+	...
+}
+...
+
+// Abstract Interfaces
+
+type <InterfaceName> interface {
+	<MethodName>(<arguments>) <ResultType>
+	...
+}
+...
+
+// PACKAGE CLASSES
+
+// Primitive Classes
+
+// This singleton defines a unique name space for the <ClassName> primitive
+// class.
 var <ClassName> = &<className>s_{
 	<className>_(...), // This is a class constant.
 	...
 }
+...
+
+// Structured Classes
+
+// This singleton defines a unique name space for the <ClassName> structured
+// class.
+var <ClassName> = &<className>s_{
+	<className>_(...), // This is a class constant.
+	...
+}
+...
 
 // PACKAGE FUNCTIONS
 
+// Public Functions
+
 // This function returns...
-func <FunctionName>(<arguments>) <resultType> {
-	var <result> <resultType>
+func <FunctionName>(<arguments>) <ResultType> {
+	var <result> <ResultType>
 	...
 	return <result>
 }
 ...
 
+// Private Functions
+
 // This private function returns...
-func <privateFunctionName>(<arguments>) <resultType> {
-	var <result> <resultType>
+func <privateFunctionName>(<arguments>) <ResultType> {
+	var <result> <ResultType>
 	...
 	return <result>
 }
