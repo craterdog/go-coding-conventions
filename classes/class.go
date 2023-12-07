fb2c5eef34fcb104fd9b94b0aa961be3a27979b6
Derk Norton <derk.norton@gmail.com>
2023-12-07 13:26:26 -0700

    Updated the class templates.

--- classes/class.go
+++ classes/class.go
@@ -16,7 +16,7 @@ import (
 
 // CLASS NAMESPACE
 
-// This private type defines the namepace structure associated with the constants,
+// This private type defines the namespace structure associated with the constants,
 // constructors and functions for the <className> class namespace.
 type <className>Class_[<parameterTypes>] struct {
 	<classConstantName> <AbstractType>
